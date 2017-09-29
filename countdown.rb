f1f4368afd55e7de3fcce35e811bcbd6b816f328
Alicia Yu <alicia.yu.16@gmail.com>
2017-09-29 19:06:39 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,17 @@
 #write your code here
 
 def countdown
-  countdown-to-midnight = 1
+  countdown-to-midnight = 0
   while countdown-to-midnight < 10
-    countdown-to-midnight += 1
     puts "#{countdown-to-midnight}SECOND(S)!"
+    countdown-to-midnight += 1
   else countdown-to-midnight > 10
     puts "HAPPY NEW YEAR!"
   end
+end
+
+counter = 0
+until counter == 20
+  puts "The current number is less than 20."
+  counter += 1
 end
