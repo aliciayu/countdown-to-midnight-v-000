b452154f8139cc4dc999c9f63e05043a70fa2240
Alicia Yu <alicia.yu.16@gmail.com>
2017-09-29 19:08:29 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,7 @@
 
 def countdown
   countdown-to-midnight = 10
-  until countdown-to-midnight == 0
+  until countdown-to-midnight == 1
     puts "#{countdown-to-midnight}SECOND(S)!"
     countdown-to-midnight -= 1
   end
