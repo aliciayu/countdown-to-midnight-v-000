913b8faf61db063ee11f9042f7a189b95c798584
Alicia Yu <alicia.yu.16@gmail.com>
2017-09-29 19:02:17 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,17 @@
 #write your code here
+def using_while
+  levitation_force = 6
+  while levitation_force < 10
+     levitation_force += 1
+     puts "Wingardium Leviosa"
+  end
+end
 
 
 def countdown
-  seconds = 1
-  until seconds == 10
-    puts "#{seconds} SECOND(S)!".each
-    seconds += 1
-    "#{} seconds"
+  countdown-to-midnight = 1
+  while countdown-to-midnight < 10
+    countdown-to-midnight += 1
+    puts "#{countdown-to-midnight}SECOND(S)!"
   end
 end
