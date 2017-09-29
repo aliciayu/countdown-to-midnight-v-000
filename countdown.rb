68fd23c0166e5c64fd9bbd12fb28d4c52a011769
Alicia Yu <alicia.yu.16@gmail.com>
2017-09-29 19:06:53 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -5,7 +5,5 @@ def countdown
   while countdown-to-midnight < 10
     puts "#{countdown-to-midnight}SECOND(S)!"
     countdown-to-midnight += 1
-  else countdown-to-midnight > 10
-    puts "HAPPY NEW YEAR!"
   end
 end
