d11f90666555bebb69b4d39032fd8b5394f7565d
Alicia Yu <alicia.yu.16@gmail.com>
2017-09-29 19:26:32 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,7 @@
 
 def countdown(countdown_to_midnight)
   while countdown_to_midnight > 0
-    "#{countdown_to_midnight}SECOND(S)!"
+    puts "#{countdown_to_midnight}SECOND(S)!"
     countdown_to_midnight -= 1
   end
   "HAPPY NEW YEAR!"
