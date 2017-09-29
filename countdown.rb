ffe24678b224d1a639462018bdfb74e97d9e759f
Alicia Yu <alicia.yu.16@gmail.com>
2017-09-29 19:28:17 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
 def countdown(countdown_to_midnight)
-  while countdown_to_midnight > 0
+  while countdown_to_midnight > 0.stdout
     puts "#{countdown_to_midnight}SECOND(S)!"
-    countdown_to_midnight -= 1.stdout
+    countdown_to_midnight -= 1
   end
   "HAPPY NEW YEAR!"
 end
