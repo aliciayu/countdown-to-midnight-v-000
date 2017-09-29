ca80b09b3f809f555eeb54c78d5ef83e9d2e36f9
Alicia Yu <alicia.yu.16@gmail.com>
2017-09-29 19:59:11 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,16 @@
 #write your code here
 
-def countdown(countdown_to_midnight)
-  while countdown_to_midnight > 0
-    puts "#{countdown_to_midnight}SECOND(S)!"
-    countdown_to_midnight -= 1
+def countdown(10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
+  countdown.each do|n|
+    puts "#{n}SECOND(S)!"
   end
 #  "HAPPY NEW YEAR!"
+end
+
+names = [ "Fred", "Bob", "Jim" ]
+
+names.each do|n|
+  puts "Hello #{n}"
 end
 
 #def countdown_with_sleep(countdown_to_midnight)
