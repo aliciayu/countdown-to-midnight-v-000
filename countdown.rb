3ee742251db7a930652ad384da77a6a5ab51c6e8
Alicia Yu <alicia.yu.16@gmail.com>
2017-09-29 18:53:44 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,5 +1,16 @@
 #write your code here
 
 def countdown
-  "<n> SECOND(S)"
+  n = 1
+  while n < 10
+    puts "#{{n} SECOND(S)}"
+    n += 1
+  end
+end
+
+def countdown_with_sleep
+  x = 1
+  while n > 5
+    n -= 5
+  end
 end
